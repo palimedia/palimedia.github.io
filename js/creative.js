ff5ff06f3f09a75816806d43a3972817fab9a0fb
Aeyd <adi.pali@hotmail.com>
2022-06-18 19:50:55 +0200

    minor

--- js/creative.js
+++ js/creative.js
@@ -7,8 +7,24 @@
 (function($) {
     "use strict"; // Start of use strict
 
+    $(document).ready(function() {
+        $('html, body').hide();
+
+        if (window.location.hash) {
+            setTimeout(function() {
+                $('html, body').scrollTop(0).show();
+                $('html, body').animate({
+                    scrollTop: $(window.location.hash).offset().top
+                    }, 1000)
+            }, 0);
+        }
+        else {
+            $('html, body').show();
+        }
+    });
+
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function(event) {    
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
